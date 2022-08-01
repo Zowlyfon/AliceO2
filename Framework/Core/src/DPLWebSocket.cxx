53e380f9b293ca12fa47d169784fd49987918d2f
Drew Mason-Laurence <drew@zowlyfon.net>
2022-08-01 15:33:30 +0200

    Clean up code and remove comments

--- Framework/Core/src/DPLWebSocket.cxx
+++ Framework/Core/src/DPLWebSocket.cxx
@@ -218,8 +218,6 @@ void remoteGuiCallback(uv_timer_s* ctx)
   GuiRenderer* renderer = reinterpret_cast<GuiRenderer*>(ctx->data);
   assert(renderer);
 
-  LOG(info) << "remoteGuiCallback";
-
   void* frame = nullptr;
   void* draw_data = nullptr;
   int size;
@@ -239,85 +237,10 @@ void remoteGuiCallback(uv_timer_s* ctx)
 
     auto header = o2::header::get<o2::header::DataHeader*>(headerString.c_str());
     renderer->gui->plugin->pollGUIPreRender(renderer->gui->window, (float)frameLatency / 1000000000.0f);
-    //draw_data = renderer->gui->plugin->pollGUIRender(renderer->gui->callback);
 
     std::function<void()> webGUICallback = [ctx, renderer, header, data](){ SpyServiceHelpers::webGUI(ctx, renderer, header, data); };
 
     draw_data = renderer->gui->plugin->pollGUIRender(webGUICallback);
-
-    /*
-    draw_data = renderer->gui->plugin->pollGUIRender([ctx, renderer, header, data](){
-      if (header != nullptr) {
-        ImGui::Begin(fmt::format("Spy {}", header->dataDescription.str).c_str());
-        ImGui::CollapsingHeader("Actions", ImGuiTreeNodeFlags_DefaultOpen);
-        if (ImGui::Button("Skip 10s")) {
-          renderer->enableAfter = uv_now(ctx->loop) + 10000;
-          uv_stop(ctx->loop);
-        }
-        if (ImGui::Button("Previous Frame")) {
-          renderer->handler->mServerContext->registry->get<SpyService>().selectedFrame--;
-        }
-        if (ImGui::Button("Next Frame")) {
-          renderer->handler->mServerContext->registry->get<SpyService>().selectedFrame++;
-        }
-        if (ImGui::CollapsingHeader("Header", ImGuiTreeNodeFlags_DefaultOpen)) {
-          ImGui::Columns(2);
-          ImGui::Text("sMagicString: %d", header->sMagicString);
-          ImGui::Text("sVersion: %d", header->sVersion);
-          ImGui::Text("sHeaderType: %s", header->sHeaderType.str);
-          ImGui::Text("sSerializationMethod: %s", header->sSerializationMethod.str);
-          ImGui::NextColumn();
-          ImGui::Text("Header size: %d", header->headerSize);
-          ImGui::Text("Payload Size: %d", header->payloadSize);
-          ImGui::Text("Header Version: %d", header->headerVersion);
-          ImGui::Text("flagsNextHeader: %d", header->flagsNextHeader);
-          ImGui::Text("serialization: %s", header->serialization.str);
-          ImGui::Text("Data Description: %s", header->dataDescription.str);
-          ImGui::Text("Data Origin: %s", header->dataOrigin.str);
-          ImGui::Text("Run Number: %d", header->runNumber);
-          ImGui::Text("Sub Specification: %s", header->subSpecification);
-
-          ImGui::Columns(1);
-        }
-
-        if (ImGui::CollapsingHeader("Payload", ImGuiTreeNodeFlags_DefaultOpen)) {
-          ImGui::BeginChild("##ScrollingRegion", ImVec2(0, 430), false, ImGuiWindowFlags_HorizontalScrollbar);
-
-          if (data != nullptr && !data.empty() && ImGui::BeginTable("Data", 5, ImGuiTableFlags_Borders)) {
-            ImGui::TableSetupColumn("");
-            ImGui::TableSetupColumn("#0");
-            ImGui::TableSetupColumn("#1");
-            ImGui::TableSetupColumn("#2");
-            ImGui::TableSetupColumn("#3");
-            ImGui::TableHeadersRow();
-            for (int row = 0; row < data.length() / 4; row++) {
-              ImGui::TableNextRow();
-              ImGui::TableSetColumnIndex(0);
-              ImGui::Text("%04d", row * 4);
-
-              for (int column = 1; column < 5; column++) {
-                std::stringstream hex;
-                if (data.length() > row * 4) {
-                  ImGui::TableSetColumnIndex(column);
-                  unsigned char dataElem = data[row * 4 + (column - 1)];
-                  hex << std::hex << (int)dataElem;
-                }
-                std::string hexStr = hex.str();
-                if (hexStr.length() == 1) {
-                  hexStr.insert(0, "0");
-                }
-                ImGui::Text("%s", hexStr.c_str());
-              }
-            }
-            ImGui::EndTable();
-          }
-          ImGui::EndChild();
-        }
-
-        ImGui::End();
-      }
-    });
-     */
 
     renderer->gui->plugin->pollGUIPostRender(renderer->gui->window, draw_data);
   } else {
@@ -385,10 +308,6 @@ void WSDPLHandler::endHeaders()
         break;
       }
     }
-  } else if (false) {
-    //LOG(debug) << "Driver connected to PID : " << header->second;
-    mHandler = std::make_unique<ControlWebSocketHandler>(*mServerContext);
-    mHandler->headers(mHeaders);
   } else {
     if (getenv("DPL_DRIVER_REMOTE_GUI")) {
       LOG(info) << "Connection not bound to a PID";
