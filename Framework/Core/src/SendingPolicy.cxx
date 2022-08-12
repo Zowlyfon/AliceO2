659858938cdd9ad0b2a604de421e78d4016b6a6a
Drew Mason-Laurence <drew@zowlyfon.net>
2022-08-12 11:22:28 +0200

    Remove Comments

--- Framework/Core/src/SendingPolicy.cxx
+++ Framework/Core/src/SendingPolicy.cxx
@@ -39,38 +39,6 @@ std::vector<SendingPolicy> SendingPolicy::createDefaultPolicies()
 
               registry.get<SpyService>().parts = &parts;
 
-              /*
-              int i = 0;
-
-              while (i < parts.Size()) {
-                std::string headerString((char*)parts[i].GetData(), parts[i].GetSize());
-                auto header = o2::header::get<o2::header::DataHeader*>(headerString.c_str());
-
-                SpyGuiData spyGuiData;
-
-                registry.get<SpyService>().sendHeader(headerString);
-                spyGuiData.header = headerString;
-
-                int payloadParts = (int)header->splitPayloadParts;
-
-                int lastPart = i + payloadParts;
-
-                while (i < lastPart) {
-                  i++;
-                  std::string dataString((char*)parts[i].GetData(), parts[i].GetSize());
-                  registry.get<SpyService>().sendData(dataString, i);
-                  spyGuiData.data.push_back(dataString);
-                }
-
-                registry.get<SpyService>().spyGuiData.push_back(spyGuiData);
-
-                i++;
-              }
-              */
-
-
-
-
               auto *channel = proxy.getOutputChannel(channelIndex);
               auto timeout = 1000;
               auto res = channel->Send(parts, timeout);
